5ea2ec4cbbf2fff6353c16bdf0d583a0be81fd0f
ella choy <egal21de@gmail.com>
2023-10-01 23:56:43 +0200

    Erstellt neue Context für Job-info

---
+++ src/content/jobInfoContent.jsx
@@ -0,0 +1,23 @@
+const jobInfo = [
+    {
+        path:"http://www.ryze-digital.de",
+        companyName: 'Ryze Digital (Electronic-Minds)Deutschland GmbH',
+        title:'als Junior Frontend Web Entwickler',
+        subText:'Mai 2022 - heute'      
+    },
+    {
+        path:"https://www.jato.com/germany/",
+        companyName: 'Jato Dynamics Deutschland GmbH',
+        title:'als Automotive Data Analyst',
+        subText:'Jun 2018 - März 2021'      
+    },
+    {
+        path:"https://www.mobisparts.eu/",
+        companyName: 'Hyundai Mobis Parts Europe N.V. Branch Germany',
+        title:' als Automotive Data Analyst',
+        subText:'Dez 2013 - Juni 2017' 
+    },
+
+];
+
+export default jobInfo;
