5182448d042b16dc9deb7305c8d63d1666284088
ella choy <egal21de@gmail.com>
2023-10-03 01:32:52 +0200

    Lagert Prokects Info in einem separaten Komponente aus

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ExternalLink } from 'react-external-link';
 import Content from './elements/content';
+import companyProject from '../content/companyProjectContent';
 
 export default function Project() {
     return (
@@ -11,45 +12,20 @@ export default function Project() {
                 />
                 <div className="add-info join-projects">
                      <Content
-                    title='Beteiligte Firmen Projekte'               
+                        title='Beteiligte Firmen Projekte'               
                     />
                     <ul>
-                         <li>
-                            <ExternalLink href='https://www.klueber.com/de/de/'>
-                               <h6 className="h6 headline">Klüber Lubrication</h6>
-                            </ExternalLink> 
-                             <p className="sub-title">Wartungsseite</p>                   
-                            <p className="sub-text">Sep 2022 - Sep 2022</p>
-                        </li>
-                        <li>
-                            <ExternalLink href='https://www.diehl.com/group/de/'>
-                               <h6 className="h6 headline">Diehl</h6>
-                            </ExternalLink> 
-                             <p className="sub-title">Website Relaunch</p>                   
-                            <p className="sub-text">Dez 2022 - März 2023</p>
-                        </li>
-                        <li>
-                             <h6 className="h6 headline">Diehl-Intranet</h6>
-                             <p className="sub-title">Intranet Nacharbeitung</p>                   
-                            <p className="sub-text">Aug 2023 - Sep 2023</p>
-                        </li>
-                        <li>
-                            <h6 className="h6 headline">Ribe-Intranet</h6>
-                             <p className="sub-title">Intranet Nacharbeitung</p>                   
-                            <p className="sub-text">Sep 2023 - Sep 2023</p>
-                        </li>
-                        <li>
-                            <h6 className="h6 headline">Deutshce Extrakt Cafea-Intranet</h6>
-                             <p className="sub-title">Intranet Relaunch</p>                   
-                            <p className="sub-text">April 2023 - heute</p>
-                        </li>
-
+                        {companyProject.map((companyProject, index) =>(
+                            <li key={index}>
+                                <ExternalLink href={companyProject.path}>
+                                    <h6 className="h6 headline">{companyProject.headline}</h6>
+                                </ExternalLink> 
+                                <p className="sub-title">{companyProject.subTitle}</p>                   
+                                <p className="sub-text">{companyProject.subText}</p>
+                            </li>
+                        ))}                       
                     </ul>
-                   
-
                 </div>
-                
-
             </div>
         </section>
     );
---
+++ src/content/companyProjectContent.jsx
@@ -0,0 +1,31 @@
+const companyProject = [
+    {
+        path: "https://www.klueber.com/de/de/",
+        headline: "Klüber Lubrication",
+        subTitle: "Wartungsseite",
+        subText: "Sep 2022 - Sep 2022",
+    },
+    {
+        path: "https://www.diehl.com/group/de/",
+        headline: "Diehl Group",
+        subTitle: "Website Relaunch",
+        subText: "Dez 2022 - März 2023",
+    },
+    {
+        headline: "Diehl Group-Intranet",
+        subTitle: "Intranet Nacharbeitung",
+        subText: "Sep 2023 - Sep 2023",
+    },
+    {
+        headline: "Ribe Group-Intranet",
+        subTitle: "Intranet Nacharbeitung",
+        subText: "Sep 2023 - Sep 2023",
+    },
+    {
+        headline: "CAFEA-Group-Intranet",
+        subTitle: "Intranet Relaunch",
+        subText: "April 2023 - heute",
+    },
+];
+
+export default companyProject;
