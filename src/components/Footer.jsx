92008088460f97c200f86db3ab6f1f0f9f6fbb75
ella choy <egal21de@gmail.com>
2023-10-03 01:47:52 +0200

    Lagert footer-content in einem separaten Komponent aus

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,27 @@
 import React from 'react';
 import { ExternalLink } from 'react-external-link';
-import {BsLinkedin} from 'react-icons/bs';
-import {BsGithub} from 'react-icons/bs';
-import {AiOutlineMail} from 'react-icons/ai';
+import footerContent from '../content/footerContent';
 
 export default function Footer() {
   return (
        <footer id="footer" className="section page-footer">
         <div className="wrapper">
             <div className="container">
-                <ul className="contact container">               
-                    <li>
-                        <ExternalLink 
-                            href="https://www.linkedin.com/in/ellachoy/" 
-                            className="has-icon">
-                            <BsLinkedin/>                           
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://github.com/ellachoy" 
-                            className="has-icon">
-                            <BsGithub />
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink 
-                            href="mailto: euiseok.choy@web.de" 
-                            className="has-icon email">
-                            <AiOutlineMail />     
-                        </ExternalLink>
-                    </li>
+                <ul className="contact container">
+                    {footerContent.map((footerContent, index) =>(
+                        <li key={index}>
+                            <ExternalLink 
+                                href={footerContent.path} 
+                                className="has-icon">
+                                {footerContent.icon}                          
+                            </ExternalLink>
+                        </li>
+                    ))}               
                 </ul>
             </div>
             <div className="divide">
                 <span className="copyright">&copy; EllaChoys - 2023</span>
             </div>
-
         </div>
     </footer>
 
---
+++ src/content/footerContent.jsx
@@ -0,0 +1,20 @@
+import {BsLinkedin} from 'react-icons/bs';
+import {BsGithub} from 'react-icons/bs';
+import {AiOutlineMail} from 'react-icons/ai';
+
+const footer = [
+    {
+        path: "https://www.linkedin.com/in/ellachoy/",
+        icon: <BsLinkedin/>,  
+    },
+    {
+        path: "https://github.com/ellachoy",
+        icon: <BsGithub />,  
+    },
+    {
+        path: "mailto: euiseok.choy@web.de" ,
+        icon: <AiOutlineMail /> ,  
+    },
+];
+
+export default footer;
