5ae460405bb63535e1a41ec7f33b2d99bbf7b630
ella choy <egal21de@gmail.com>
2023-10-06 18:27:06 +0200

    Update skillsContent.jsx

--- src/content/skillsContent.jsx
+++ src/content/skillsContent.jsx
@@ -1,19 +1,19 @@
 const skills = [
     {
         headline: 'HTML',
-        description: 'Umgang mit semantischem HTML5/ barrierefreien Markup',
+        description: 'Guter Umgang mit semantischem HTML5',
     },
     {
         headline: 'CSS/SCSS',
-        description: 'Praktische Erfahrung im Schreiben von CSS mittels Dart sass/ Umsetzung nicht für Mobile First sondern für alle Viewport',
+        description: 'Praktische Erfahrung im Schreiben von CSS mittels Dart sass/ Umsetzung nicht nur für Mobile First sondern für alle Viewport',
     },
     {
         headline: 'Javascript',
         description: 'Praktische Erfahrung von nativem Javascript/ Programmierung von nativen wiederverwendbaren JavaScript-Modulen',
     },
     {
-        headline: 'BasisKenntnisse',
-        description: 'PHP (mit Berufspraxis) / React  ',
+        headline: 'Basiswissen',
+        description: 'PHP (mit Berufspraxis) / React / Verständnis für Mobile First und Atomic Design  ',
     },
     {
         headline: 'Tools',
