9823e522c8c057539ecb41c2962d91896684e2ab
ella choy <egal21de@gmail.com>
2024-06-11 00:26:11 +0200

    update Stile für Home und Header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,31 @@
-import { InView } from 'react-intersection-observer';
-import Symbols from './elements/symbols-list';
+import { InView } from 'react-intersection-observer'
+import Symbols from './elements/symbols-list'
 
 export default function Home() {
   return (
     <InView>
-      {({inView, ref}) => (
-        <section ref={ref} id="home" className="home">
-          <div className={inView ? "active": "inactive"}>
-            <div className="wrapper">
-              <div className="content">
-                  <h1 className="h1 content-text">
-                    <div >
-                      <div className="phrase">
-                      <span>
-                          Frontend Web Entwicklung ist,
-                      </span>                
+      {({ inView, ref }) => (
+        <section ref={ref} id='home' className='home'>
+          <div className={inView ? 'active' : 'inactive'}>
+            <div className='wrapper'>
+              <div className='content'>
+                <h1 className='h1 content-text'>
+                  <div>
+                    <div className='phrase'>
+                      <span>Web Entwicklung ist,</span>
                     </div>
-                    <div className="phrase">
-                          <span>Synergie von Digitaler Kommunikation,</span>
-                          <span> Kunst und Technologien</span>
-                      </div>  
-                    </div>            
-                  </h1>
-                  <Symbols />
-              </div>   
+                    <div className='phrase'>
+                      <span>Synergie von Digitaler Kommunikation,</span>
+                      <span> Kunst und Technologien</span>
+                    </div>
+                  </div>
+                </h1>
+                {/* <Symbols /> */}
+              </div>
             </div>
-          </div>     
+          </div>
         </section>
-      )}       
-    </InView>  
-  );
+      )}
+    </InView>
+  )
 }
