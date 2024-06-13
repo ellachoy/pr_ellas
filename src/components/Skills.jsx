0e866c583515dc38d97edeb720108a4fe11f045e
ella choy <egal21de@gmail.com>
2024-06-13 23:21:19 +0200

    update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,35 +1,33 @@
-import { InView } from 'react-intersection-observer';
-import skills from '../content/skillsContent';
-import Content from './elements/content';
-
+import { InView } from 'react-intersection-observer'
+import skills from '../content/skillsContent'
+import Content from './elements/content'
 
 export default function Skills() {
-    return (
-        <InView>
-        {({ inView, ref}) => (
-            <section ref={ref} id="skills" className="skills">
-                <div className={inView ? "is-in-viewport": "is-out-of-viewport"}>
-                    <div className="wrapper">
-                        <Content
-                            title='Skills'
-                            description='Ich habe seit Mai 2022 Berufserfahrung in der Web-Entwicklung mit PHP, Native Javascript, Dart Sass, Hauseigene Content Management System und Entwicklung von Styleguid durch agile Methoden gesammelt.
-                            Dazu besitze Ich zwei gute Augen für´s Ästhetik und Design'            
-                        />            
-                        <ul className="skills-list">
-                            {skills.map((skill, index) => (
-                                <li key={index} 
-                                >
-                                    <div className="list-container">
-                                        <h5 className="headline h5">{skill.headline}</h5>
-                                        <p>{skill.description}</p>
-                                    </div>  
-                                </li>                      
-                            ))}          
-                        </ul> 
-                    </div>         
-                </div>                          
-            </section>
-        )}
+  return (
+    <InView>
+      {({ inView, ref }) => (
+        <section ref={ref} id='skills' className='skills'>
+          <div className={inView ? 'is-in-viewport' : 'is-out-of-viewport'}>
+            <div className='wrapper'>
+              <Content
+                title='Skills'
+                description='Berufserfahrung in der Webentwicklung mit PHP, Native Javascript, Dart Sass, CMS, React und Entwicklung von Styleguid und Storybook durch agile Methoden gesammelt.
+                            Dazu besitze Ich zwei gute Augen für´s Ästhetik und Design'
+              />
+              <ul className='skills-list'>
+                {skills.map((skill, index) => (
+                  <li key={index}>
+                    <div className='list-container'>
+                      <h5 className='headline h5'>{skill.headline}</h5>
+                      <p>{skill.description}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </section>
+      )}
     </InView>
-    );
+  )
 }
--- src/content/skillsContent.jsx
+++ src/content/skillsContent.jsx
@@ -1,32 +1,37 @@
 const skills = [
-    {
-        headline: 'HTML',
-        description: 'Guter Umgang mit semantischem HTML5',
-    },
-    {
-        headline: 'CSS/SCSS',
-        description: 'Praktische Erfahrung im Schreiben von CSS mittels Dart sass / Umsetzung in Mobile First und alle anderen Viewports',
-    },
-    {
-        headline: 'Javascript',
-        description: 'Praktische Erfahrung von nativem Javascript/ Programmierung von nativen wiederverwendbaren JavaScript-Modulen',
-    },
-    {
-        headline: 'Basiswissen',
-        description: 'PHP (mit Berufspraxis) / React / Verständnis für Mobile First und Atomic Design  ',
-    },
-    {
-        headline: 'Tools',
-        description: 'Microsoft Teams / Jira / Confluence / Trello/ Figma / GitLab / GitHub / Yarn / Npm/ BrowserStack / Git / VSCode / IDE-PHPStorm / CMS system (bisher hauseigenes CMS System) ',
-    },
-    {
-        headline: 'Test',
-        description: 'CanIUse / Validator.w3.org / BrowserStack / Ghostinspector  ',
-    },
-    {
-        headline: 'CI/CD',
-        description: 'Jenkins / Netlify',
-    },
-];
+  {
+    headline: 'HTML',
+    description: 'Guter Umgang mit semantischem HTML5',
+  },
+  {
+    headline: 'CSS / SCSS',
+    description:
+      'Praktische Erfahrung im Schreiben von CSS mittels Dart sass / Umsetzung in Mobile First und alle anderen Viewports',
+  },
+  {
+    headline: 'Javascript',
+    description:
+      'Praktische Erfahrung von nativem Javascript/ Programmierung von nativen wiederverwendbaren JavaScript-Modulen',
+  },
+  {
+    headline: 'Basiswissen',
+    description:
+      'PHP (mit Berufspraxis) / React (mit Berufspraxis)',
+  },
+  {
+    headline: 'Tools',
+    description:
+      'Microsoft Teams / Jira / Confluence / Trello/ Figma / GitLab / GitHub / Yarn / Npm/ BrowserStack / Git / VSCode / IDE-PHPStorm / CMS system (bisher hauseigenes CMS System) ',
+  },
+  {
+    headline: 'Test',
+    description:
+      'CanIUse / Validator.w3.org / BrowserStack / Ghostinspector / Jest ',
+  },
+  {
+    headline: 'CI / CD',
+    description: 'Jenkins / Netlify',
+  },
+]
 
-export default skills;
+export default skills
